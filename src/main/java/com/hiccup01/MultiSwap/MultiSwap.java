7bba60f76d7af23718ca267e904c455f168758c1
serverhiccups <hiccup@hiccup01.com>
2020-03-08 10:47:50 +1300

    Add distribution command and fixed some bugs with the picking of the swap locations.

--- src/main/java/com/hiccup01/MultiSwap/MultiSwap.java
+++ src/main/java/com/hiccup01/MultiSwap/MultiSwap.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,6 +15,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public final class MultiSwap extends JavaPlugin implements Listener {
@@ -67,7 +70,17 @@ public final class MultiSwap extends JavaPlugin implements Listener {
 	private void setTriggers() {
 		int minTimeBeforeSwap = getConfig().getInt("minTimeBeforeSwap");
 		int maxTimeBeforeSwap = getConfig().getInt("maxTimeBeforeSwap");
-		this.triggersLeft = ThreadLocalRandom.current().nextInt(minTimeBeforeSwap / 10, (maxTimeBeforeSwap / 10) + 1);
+		this.triggersLeft = ThreadLocalRandom.current().nextInt(minTimeBeforeSwap / 10, (maxTimeBeforeSwap / 10) + 1) - 1;
+	}
+
+	private void distributePlayers(int seed) {
+		int i = 0;
+		for(Player player : this.playingPlayers) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 5*20, 999, false, false));
+			player.teleport(new Location(getServer().getWorld("world"), (seed + 1) * (double)2000, (double)128, i * (double)2000));
+			player.getInventory().clear();
+			i++;
+		}
 	}
 
 	@Override
@@ -84,7 +97,14 @@ public final class MultiSwap extends JavaPlugin implements Listener {
 			this.gameInProgress = true;
 			this.playingPlayers = new ArrayList<>(getServer().getOnlinePlayers());
 			this.setTriggers();
-			scheduler.scheduleSyncRepeatingTask(this, new SwapPlayersTask(this), 0L, 10 * 20L);
+			scheduler.scheduleSyncRepeatingTask(this, new SwapPlayersTask(this), 10 * 20L, 10 * 20L);
+			return true;
+		} else if(command.getName().equalsIgnoreCase("distribute")) {
+			if(args.length > 0) {
+				this.distributePlayers(Integer.parseInt(args[0]));
+			} else {
+				this.distributePlayers(0);
+			}
 			return true;
 		}
 		return false;
@@ -92,15 +112,24 @@ public final class MultiSwap extends JavaPlugin implements Listener {
 
 	public void swapPlayers() {
 		if(this.triggersLeft > 0) {
+//			getServer().broadcastMessage(String.valueOf(this.triggersLeft * 10) + " seconds left.");
 			this.triggersLeft--;
 		} else {
 			ArrayList<Player> swappingPlayers = playingPlayers;
 			swappingPlayers.retainAll(getServer().getOnlinePlayers());
+			ArrayList<Player> playerLocations = new ArrayList<>(playingPlayers);
+			boolean valid = false;
+			while(!valid) {
+				Collections.shuffle(playerLocations);
+				valid = true;
+				for(int i = 0; i < swappingPlayers.size(); i++) {
+					if(swappingPlayers.get(i).equals(playerLocations.get(i))) valid = false;
+				}
+			}
 			List<Location> locations = new ArrayList<>();
 			for (int i = 0; i < swappingPlayers.size(); i++) {
-				locations.add(swappingPlayers.get(i).getLocation());
+				locations.add(playerLocations.get(i).getLocation());
 			}
-			Collections.shuffle(locations);
 			getLogger().info("Swapping!");
 			getServer().broadcastMessage("Swapping!");
 			for (int i = 0; i < swappingPlayers.size(); i++) {
